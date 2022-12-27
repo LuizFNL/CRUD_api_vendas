4a963f70fb0f257a4b86c04d0a196f650c66bdfd
Luiz Fernando <luiz.lol14@gmail.com>
2022-12-27 17:22:23 -0300

    remove "using" not used

--- Controllers/VendaController.cs
+++ Controllers/VendaController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using tech_test_payment_api.Context;
 using tech_test_payment_api.Models;
--- Models/Vendedor.cs
+++ Models/Vendedor.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace tech_test_payment_api.Models
 {
-    
+
 
     public class Vendedor
     {
